67cd0b976f828bf1d87b7bec9090b53756661c2f
Miguel Antonio <miguel.cabreja90@gmail.com>
2020-09-22 10:40:47 -0400

    reformat source

--- app/Http/Controllers/Api/AuthenticationController.php
+++ app/Http/Controllers/Api/AuthenticationController.php
@@ -13,21 +13,7 @@ use Illuminate\Support\Facades\Validator;
 class AuthenticationController extends Controller
 {
     /**
-     * Shows authenticated user information
-     *
-     * @authenticated
-     *
-     * @response 200 {
-     *     "id": 2,
-     *     "name": "Demo",
-     *     "email": "demo@demo.com",
-     *     "email_verified_at": null,
-     *     "created_at": "2020-05-25T06:21:47.000000Z",
-     *     "updated_at": "2020-05-25T06:21:47.000000Z"
-     * }
-     * @response status=400 scenario="Unauthenticated" {
-     *     "message": "Unauthenticated."
-     * }
+     * @return mixed
      */
     public function user()
     {
