710314b9e0c48a9d4d2ce2e7a1e793ba699d6823
Miguel Antonio <miguel.cabreja90@gmail.com>
2020-10-02 16:41:11 -0400

    added debug request

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -72,6 +72,7 @@ class RegisterController extends Controller
      */
     public function register(Request $request)
     {
+        dd($request->all());die;
         $this->validator($request->all())->validate();
 
         event(new Registered($user = $this->create($request->all())));
