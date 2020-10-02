fa857e773d42f651b17a61fe21895b9c152c6106
Miguel Antonio <miguel.cabreja90@gmail.com>
2020-10-02 17:10:47 -0400

    deleted debug request

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -72,7 +72,7 @@ class RegisterController extends Controller
      */
     public function register(Request $request)
     {
-        $this->validator($request->all())->validate();
+        //$this->validator($request->all())->validate();
 
         event(new Registered($user = $this->create($request->all())));
 
