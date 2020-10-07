7545b3c6af70492bfd741d143abb32064ee82fa1
Miguel Antonio <miguel.cabreja90@gmail.com>
2020-10-07 00:33:55 -0400

    added AppColorPicker componet

---
+++ resources/js/components/core/AppColorPicker.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-row justify="space-around">
+    <div class="form__field">
+      <div class="form__label">
+        <strong>{{ $vuetify.lang.t('$vuetify.component.fieldColor') }}:</strong>
+        <v-swatches
+          v-model="color"
+          inline
+          @input="input($event)"
+        />
+      </div>
+    </div>
+  </v-row>
+</template>
+
+<script>
+
+import VSwatches from 'vue-swatches'
+export default {
+  name: 'AppColorPicker',
+  components: { VSwatches },
+  props: {
+    value: {
+      type: String,
+      default: '',
+      required: true
+    }
+  },
+  data () {
+    return {
+      color: this.value
+    }
+  },
+  methods: {
+    input ($event) {
+      this.$emit('input', $event)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
