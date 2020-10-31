564da169f35c4774ce0a0182c1a4bb7a43bed223
jlbarrero <jlbarrero1990@gmail.com>
2020-10-31 19:34:42 -0400

    Merge branch 'master' into make_supplier
    # Conflicts:
    #	resources/js/locale/en.js
    #	resources/js/locale/es.js
    #	resources/js/views/article/NewArticlePage.vue

--- database/migrations/2020_10_31_220138_create_articles_shops_table.php
+++ database/migrations/2020_10_31_220138_create_articles_shops_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateVariantsShopsTable extends Migration
+class CreateArticlesShopsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,15 @@ class CreateVariantsShopsTable extends Migration
      */
     public function up()
     {
-        Schema::create('variants_shops', function (Blueprint $table) {
+        Schema::create('articles_shops', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->decimal('price',15,2);
             $table->integer('stock')->nullable();
             $table->integer('under_inventory')->nullable();
             $table->timestamps();
-
             $table->foreignUuid('articles_id')->references('id')->on('articles')
                 ->onDelete('cascade');
-            $table->foreignUuid('shop_id')->references('id')->on('shops')
-                ->onDelete('cascade');
-            $table->foreignUuid('vv_id')->references('id')->on('variants_values')
+            $table->foreignUuid('shops_id')->references('id')->on('shops')
                 ->onDelete('cascade');
         });
     }
@@ -36,6 +33,6 @@ class CreateVariantsShopsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('variants_shops');
+        Schema::dropIfExists('articles_shops');
     }
 }
